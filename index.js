91a3d9b36996bff3dc71a2fb0ed561c4fd4fdf5e
ChokunPlayZ <49745982+ChokunPlayZ@users.noreply.github.com>
2021-09-18 22:17:25 +0700

    remove unused code

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const colour = require('colour');
 const myIntents = new Intents();
 myIntents.add();
 
-const wait = require('util').promisify(setTimeout);
-
 const client = new Client({
      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS],
      presence: {
